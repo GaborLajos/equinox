ca229a965ba7859a5d7e171604fca4cad3b21e47
Mate Gabor <gabor.lajos.mate@gmail.com>
2026-09-20 15:32:39 +0200

    initial commit

---
+++ equinox_test/equinox_test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  equinox_test
+//
+//  Created by Gabor Lajos Mate on 2026. 09. 20..
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+  return 0;
+}
